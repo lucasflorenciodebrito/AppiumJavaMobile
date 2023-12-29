85051f434328957973093058490ca8fa5fcc79e5
lucasflorenciodebrito <lucasdibex@gmail.com>
2023-12-29 20:15:32 -0300

    Adding elements

---
+++ src/test/java/org/example/appiumjavamobile/modules/LoginTest.java
@@ -0,0 +1,31 @@
+package org.example.appiumjavamobile.modules;
+
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginTest {
+
+    public LoginTest(AppiumDriver driver){
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    @AndroidFindBy(id = "login_username")
+    @iOSXCUITFindBy(accessibility = "login_username")
+    private RemoteWebElement emailField;
+
+    @AndroidFindBy(id = "login_password")
+    @iOSXCUITFindBy(accessibility = "login_password")
+    private RemoteWebElement passwordField;
+
+    @AndroidFindBy(id = "login_button")
+    @iOSXCUITFindBy(accessibility = "login_button")
+    private RemoteWebElement loginButtton;
+
+
+
+}
