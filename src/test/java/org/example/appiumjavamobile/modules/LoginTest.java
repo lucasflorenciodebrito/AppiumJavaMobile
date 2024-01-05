67aeb7a570474bd0896aa402bb767740aa41cadb
lucasflorenciodebrito <lucasdibex@gmail.com>
2024-01-05 20:19:58 -0300

    Refactoring the tests to rin on the browser stack

--- src/test/java/org/example/appiumjavamobile/modules/LoginTest.java
+++ src/test/java/org/example/appiumjavamobile/modules/LoginTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import org.example.appiumjavamobile.pages.LoginPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,33 +12,39 @@ import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Locale;
 
 public class LoginTest {
 
-    public static String local = "";
-    public static String platform = "";
+
+    private  static String platform = "";
+    private static String key = "";
     private static LoginPage loginPage;
     private static AppiumDriver<RemoteWebElement> driver;
 
     @BeforeEach
     public void setup() throws MalformedURLException {
 
-        local = System.getProperty("local").toLowerCase();
         platform = System.getProperty("platform").toLowerCase();
+        key = System.getProperty("key").toLowerCase();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        URL appiumServerURL = new URL("http://localhost:4723/wd/hub");
+        URL appiumServerURL = new URL("http://hub.browserstack.com/wd/hub");
 
-        if (local.equals("local") && platform.equals("android")) {
-            capabilities.setCapability("app", "C:\\Users\\Lucas\\APK_TEST\\app-debug.apk");
-            capabilities.setCapability("deviceName", "Lucas Brito");
+        capabilities.setCapability("browserstack.user", "lucasbrito_I9bc1L");
+        capabilities.setCapability("browserstack.key", key);
+
+        if (platform.equals("android")) {
+            capabilities.setCapability("app", "bs://3fddaef2b9e30d7feba8100bb87a3820e6c4419f");
+            capabilities.setCapability("deviceName", "Google Pixel 3");
             capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("platformVersion", "13");
+            capabilities.setCapability("platformVersion", "9.0");
+            capabilities.setCapability("build", "Build 01");
             driver = new AndroidDriver<RemoteWebElement>(appiumServerURL, capabilities);
 
-        } else if (local.equals("local") && platform.equals("iOS")) {
-            capabilities.setCapability("app", "C:\\Users\\Lucas\\APK_TEST\\app-debug.apk");
+        } else if (platform.equals("iOS")) {
+            capabilities.setCapability("app", "bs://3fddaef2b9e30d7feba8100bb87a3820e6c4419f");
             capabilities.setCapability("deviceName", "iPhone 13");
             capabilities.setCapability("platformName", "iOS");
             capabilities.setCapability("platformVersion", "15.0");
@@ -47,6 +54,13 @@ public class LoginTest {
 
         loginPage = new LoginPage(driver);
 
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
